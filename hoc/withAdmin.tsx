14dd35bac876e5513c6c2d90d06dd1be02605295
AlickSadler <alicksadler@gmail.com>
2024-08-03 16:33:29 +0300

    Update

--- hoc/withAdmin.tsx
+++ hoc/withAdmin.tsx
@@ -4,12 +4,14 @@ import { ReactNode, useEffect } from "react";
 import { redirect } from "next/navigation";
 
 const AdminRoute = ({ children }: { children: ReactNode }) => {
-  if (!localStorage?.getItem("isAdmin")) {
-    redirect("/");
-  }
+  if (typeof window !== "undefined") {
+    if (!localStorage?.getItem("isAdmin")) {
+      redirect("/");
+    }
 
-  if (!localStorage?.getItem("isAdmin")) {
-    return null;
+    if (!localStorage?.getItem("isAdmin")) {
+      return null;
+    }
   }
 
   return <>{children}</>;
